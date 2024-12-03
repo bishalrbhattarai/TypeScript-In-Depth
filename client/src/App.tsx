a1d9ad4ab0c650d4162b1d7ee70f5fa822a57d39
Bishal Raj Bhattarai <bishalrajbhattarai01@gmail.com>
2024-12-03 15:04:35 +0545

    basic configuration done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,5 @@
-
-// import styled from '@emotion/styled';
-import { Box, Button, Container, Stack, Typography,styled } from '@mui/material';
-import Leftbar from './components/Leftbar';
-import Rightbar from './components/Rightbar';
-import Feed from './components/Feed';
-import Navbar from './components/Navbar';
 function App() {
-
-
-
-  return (
-    <>
-<Navbar />
-<Box>
-
-
-
-
-      <Stack direction={"row"} spacing={5} justifyContent={"space-between"}>
-        <Leftbar  />
-        <Feed />
-        <Rightbar />
-      </Stack>
-
-      </Box>
-
-   
-
-    </>
-  )
+  return <></>;
 }
 
-
-export default App
+export default App;
