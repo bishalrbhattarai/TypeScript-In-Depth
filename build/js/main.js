2f3e4772e92b63e6cf79b8a2a80148551b8faa3a
Bishal Raj Bhattarai <bishalrajbhattarai01@gmail.com>
2024-12-03 11:58:03 +0545

    working with arrays

--- build/js/main.js
+++ build/js/main.js
@@ -1,10 +1,12 @@
 "use strict";
-let myName = "John Doe";
-let val;
-val = true;
-let album = "Demo";
-album = 13;
-const sum = (a, b) => {
-    return +a + b;
-};
-console.log(sum(2, 4));
+// let myName = "John Doe";
+// let val: unknown;
+// val = true as boolean;
+// let album: any = "Demo";
+// album = 13;
+// const sum = (a: number, b: number): number => {
+//   return +a + b;
+// };
+// console.log(sum(2, 4));
+let stringArr = ["one", "two", "three", "four"];
+let guitars = ["start", "les paaul", 5150];
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,17 @@
-let myName = "John Doe";
+// let myName = "John Doe";
 
-let val: unknown;
-val = true as boolean;
+// let val: unknown;
+// val = true as boolean;
 
-let album: any = "Demo";
-album = 13;
+// let album: any = "Demo";
+// album = 13;
 
-const sum = (a: number, b: number): number => {
-  return +a + b;
-};
+// const sum = (a: number, b: number): number => {
+//   return +a + b;
+// };
 
-console.log(sum(2, 4));
+// console.log(sum(2, 4));
+
+let stringArr = ["one", "two", "three", "four"];
+
+let guitars = ["start", "les paaul", 5150];
